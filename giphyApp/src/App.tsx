7d98b904bb883e58613a82923e5e110313c94300
Nicolas Barzola <nicofranco18@hotmail.com>
2024-10-09 12:49:05 -0300

    Giphy

---
+++ giphyApp/src/App.tsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./components/ui/NavBar/NavBar"
+import { GiftList } from "./components/GiftList/GiftList";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <GiftList />
+    </>
+  )
+}
+
+export default App
